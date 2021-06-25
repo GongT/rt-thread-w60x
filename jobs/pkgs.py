f0c03313456dfbba92415f692ea48cfdd42a37aa
GongT <admin@gongt.me>
2021-06-25 12:30:46 +0800

    pkgs add auto download

--- jobs/pkgs.py
+++ jobs/pkgs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from helpers import exec_pass, die, print, ENV_ROOT, PROJECT_ROOT, PACKAGES_ROOT
+from .scons import scons
 
 help_title = '运行pkgs'
 
@@ -16,7 +17,13 @@ def main(argv):
     print(argv)
     if len(argv) > 0 and argv[0] == '--force-update':
         reset_pkgs()
+
+    envshPath = Path(ENV_ROOT).joinpath('env.sh')
+
+    if not envshPath.exists():
+        scons(['--pyconfig-silent'])
+
     argv.insert(0, "--")
-    argv.insert(0, f"source '{ENV_ROOT}/env.sh'; set -x; exec pkgs \"$@\"")
+    argv.insert(0, f"source '{envshPath.absolute().as_posix()}'; set -x; exec pkgs \"$@\"")
     argv.insert(0, "-c")
     exec_pass("bash", argv, cwd=PROJECT_ROOT)
